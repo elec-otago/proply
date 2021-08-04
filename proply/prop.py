2a65f5e0e8c58f6ce952276175c04298a74895e0
tim <tim@elec.ac.nz>
2021-08-05 10:55:31 +1200

    Add blade templates, include package resources

--- proply/prop.py
+++ proply/prop.py
@@ -18,6 +18,14 @@ from proply.blade_element import BladeElement
 
 from scipy.interpolate import PchipInterpolator, interp1d
 
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+from proply import templates
+
 logger = logging.getLogger(__name__)
 
 
